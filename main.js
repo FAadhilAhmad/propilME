1223154a943d06d1d8dcf9f88a818c3146df7c38
fadfadhil <fadhilsyawaludin5@gmail.com>
2025-07-22 21:29:31 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 let item = document.getElementById('container-identitas');
 let btnSongs = document.getElementById('favSongs');
+
 let itemAlbum = document.getElementById('container-album');
 let btnAlbum = document.getElementById('myAlbum');
 
+let itemindentitas = document.getElementById('container-jurnal');
+let btnjurnal = document.getElementById('jurnalStudi');
 
 btnSongs.addEventListener('click', function(){
     item.classList.toggle('hideIcon');
@@ -11,4 +14,9 @@ btnSongs.addEventListener('click', function(){
 
 btnAlbum.addEventListener('click', function(){
     itemAlbum.classList.toggle('hideIconDua');
+});
+
+btnjurnal.addEventListener('click', function(){
+    itemindentitas.classList.toggle('hideIconTiga');
+
 });
