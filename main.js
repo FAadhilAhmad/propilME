9247307f8722eed6639eca3c9d9bc32152bd6179
fadfadhil <fadhilsyawaludin5@gmail.com>
2025-07-23 22:15:19 +0700

    Update main.js

--- main.js
+++ main.js
@@ -9,14 +9,35 @@ let btnjurnal = document.getElementById('jurnalStudi');
 
 btnSongs.addEventListener('click', function(){
     item.classList.toggle('hideIcon');
-
+    itemAlbum.classList.add('hideIconDua');
+    itemindentitas.classList.add('hideIconTiga');
 });
 
 btnAlbum.addEventListener('click', function(){
     itemAlbum.classList.toggle('hideIconDua');
+    item.classList.add('hideIcon');
+    itemindentitas.classList.add('hideIconTiga');
 });
 
 btnjurnal.addEventListener('click', function(){
     itemindentitas.classList.toggle('hideIconTiga');
+    item.classList.add('hideIcon');
+    itemAlbum.classList.add('hideIconDua');
+});
 
+// function from comentar
+let inputComentar = document.getElementById('inputComentar');
+let outputComentar = document.getElementById('outputComentar2');
+let btnComentar = document.getElementById('btnComentar');
+
+btnComentar.addEventListener('click', function(){
+  let komentar = inputComentar.value.trim();
+
+  if (komentar !==''){
+    let paragraf = document.createElement('p');
+    paragraf.textContent = komentar;
+    outputComentar.appendChild(paragraf);
+
+    inputComentar.value = '';
+  }
 });
