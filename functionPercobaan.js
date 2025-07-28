30ec3404a99145b27f21574c512f847491737ac1
fadfadhil <fadhilsyawaludin5@gmail.com>
2025-07-28 21:49:04 +0700

    Add files via upload

---
+++ functionPercobaan.js
@@ -0,0 +1,34 @@
+// deklarasi 
+let nama = 'fadhil';
+let umur = '20';
+let hobi = 'belajar';
+let takut = 'di tinggalkan ';
+let kuat = 'tekad saya kuat dan pantang menyerah ';
+let favorit = 'karakter favorit saya adalah asta dan naruto';
+
+// deklarasi functiom 
+perkenalan()
+ceritaSedikit()
+
+// eksekusi data 
+function perkenalan() {
+    console.log('Nama saya ' + nama +' dan umur saya '+ umur);
+};
+
+function ceritaSedikit(){
+    console.log('Saya paling takut '+ takut + ' dan karakter anime favorit saya '+ favorit);
+};
+
+// pengunaan data berulang
+perkenalan()
+perkenalan()
+ceritaSedikit()
+ceritaSedikit()
+
+var halo = 'haii guyss';
+haloAku()
+
+function haloAku(){
+    console.log( halo );
+};
+co
